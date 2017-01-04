1709fba4e382613da541ff685b30692a2c642ba9
zhangxin <zhang.x@outlook.com>
2017-01-04 22:17:01 +0800

    文档重命名

---
+++ BlogEngine/BlogEngine.NET/Custom/Extensions/AIQiniu.cs
@@ -0,0 +1,203 @@
+﻿/****************************** 模块头 ******************************\
+ * 
+ * 模块名称:  AIQiniu.cs
+ * 创建时间:  2015/2/24
+ * 版权：Copyright (c) (http://zhangx.net) Corporation.
+ * 
+ * 
+ * 实现七牛云镜像。
+ * 
+ * 
+ * This source is subject to the Microsoft Public License.
+ * See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+ * All other rights reserved.
+ * 
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Net;
+
+using BlogEngine.Core.Web.Controls;
+using BlogEngine.Core;
+using System.Collections;
+using BlogEngine.Core.Web.Extensions;
+
+/// <summary>
+/// 插件名称、版本、作者、优先级。
+/// </summary>
+/// 
+[Extension("七牛云镜像", "1.0", "<a href=\"http://www.zhangx.net\">zhangx.net</a>", 20)]
+public class AIQiniu
+{
+    private static string localHost=""; // 博客域名
+    private static string qiniuHost=""; // 七牛域名
+    private static string cdnExts=""; // 扩展名（使用|分隔）
+	//private static string CDN_DIRS   = "wp-content|wp-includes"; // 目录（使用|分隔）
+
+    private const string extensionName = "AIQiniu";
+    private static readonly object syncRoot = new object();
+    private static Dictionary<Guid, ExtensionSettings> blogsSettings = new Dictionary<Guid, ExtensionSettings>();
+
+
+    public AIQiniu()
+    {
+        InitSettings();
+        Post.Serving += new EventHandler<ServingEventArgs>(Post_Serving);
+    }
+
+    #region Settings
+    private static void InitSettings()
+    {
+        // call Settings getter so default settings are loaded on application start.
+        var s = Settings;
+    }
+
+    private static ExtensionSettings Settings
+    {
+        get
+        {
+            Guid blogId = Blog.CurrentInstance.Id;
+            if (!blogsSettings.ContainsKey(blogId))
+            {
+                lock (syncRoot)
+                {
+                    if (!blogsSettings.ContainsKey(blogId))
+                    {
+                        blogsSettings[blogId] = LoadExtensionSettingsForBlogInstance();
+                    }
+                }
+            }
+
+            return blogsSettings[blogId];
+        }
+    }
+
+    private static ExtensionSettings LoadExtensionSettingsForBlogInstance()
+    {
+        ExtensionSettings initialSettings = new ExtensionSettings(extensionName);
+        #region Help
+//        initialSettings.Help = @"
+//        <p>Build on <a href=""http://mediaelement.js.com/"">MediaElement.js</a>, the HTML5 video/audio player.</p>
+//
+//        <ol>
+//	        <li>Upload media files to your /media/ folder</li>
+//	        <li>Add short code to your media: [video src=""myfile.mp4""] for video and [audio src=""myfile.mp3""] for audio</li>
+//	        <li>Customize with the following parameters:
+//		        <ul>
+//			        <li><b>width</b>: The exact width of the video</li>
+//			        <li><b>height</b>: The exact height of the video</li>
+//			        <li><b>autoplay</b>: Plays the video as soon as the webpage loads</li>
+//		        </ul>
+//	        </li>
+//	        <li>You can also specify multiple file formats and codecs 
+//		        <ul>
+//			        <li><b>mp4</b>: H.264 encoded MP4 file</li>
+//			        <li><b>webm</b>: VP8/WebM encoded file</li>
+//			        <li><b>ogg</b>: Theora/Vorbis encoded file</li>
+//		        </ul>
+//	        </li>
+//        </ol>
+//
+//        <p>A complete example:<br />
+//        [code mp4=""myfile.mp4"" webm=""myfile.webm"" ogg=""myfile.ogg"" width=""480"" height=""360""]
+//        </p>
+//
+//        <p>Supported formats</p>
+//        <ul>
+//	        <li><b>MP4/MP3</b>: Native HTML5 for IE9, Safari, Chrome; Flash in IE8, Firefox, Opera</li>
+//	        <li><b>WebM</b>: HTML5 for IE9, Chrome, Firefox, Opera; Flash in IE8 (coming in Flash 11)</li>
+//	        <li><b>FLV</b>: Flash fallback</li>
+//	        <li><b>WMV/WMA</b>: Silverlight fallback</li>
+//        </ul>
+//        ";
+        #endregion
+        initialSettings.IsScalar = true;
+
+
+        initialSettings.AddParameter("local_host", "博客域名");
+        initialSettings.AddValue("local_host", localHost);
+
+        initialSettings.AddParameter("qiniu_host", "七牛绑定域名");
+        initialSettings.AddValue("qiniu_host", qiniuHost);
+
+        initialSettings.AddParameter("cdn_exts", "扩展名（使用|分隔");
+        initialSettings.AddValue("cdn_exts", cdnExts);
+
+        return ExtensionManager.InitSettings(extensionName, initialSettings);
+    }
+    #endregion
+
+    /// <summary>
+    /// 调用文章时触发。
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void Post_Serving(object sender, ServingEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Body))
+        {
+            if (e.Location == ServingLocation.PostList || e.Location == ServingLocation.SinglePost)
+            {
+
+                GetHrefUrl(e);
+                GetSrcUrl(e);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 取得HTML中所有的href
+    /// </summary>
+    /// <returns></returns>
+    private static string[] GetHrefUrl(ServingEventArgs e)
+    {
+        string href = string.Format("href=(?:\"|\')?(?<url>[^>]*[^/].(?:{0}))(?:\"|\')?", Settings.GetSingleValue("cdn_exts"));
+        Regex r = new Regex(href, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        MatchCollection matches = r.Matches(e.Body);
+
+        int i = 0;
+        string[] sUrlList = new string[matches.Count];
+
+        // 取得匹配项列表
+        foreach (Match match in matches)
+        {
+            string m = match.Groups["url"].Value;
+            sUrlList[i++] = m;
+            e.Body = e.Body.Replace(m, (Settings.GetSingleValue("qiniu_host") + m).Replace("?", HttpUtility.UrlEncode("?")));
+        }
+        return sUrlList;
+    }
+
+    /// <summary>
+    /// 取得HTML中所有的src
+    /// </summary>
+    /// <returns></returns>
+    private static string[] GetSrcUrl(ServingEventArgs e)
+    {
+        string src = string.Format("src=(?:\"|\')?(?<url>[^>]*[^/].(?:{0}))(?:\"|\')?", Settings.GetSingleValue("cdn_exts"));
+        Regex r = new Regex(src, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        MatchCollection matches = r.Matches(e.Body);
+
+        int i = 0;
+        string[] sUrlList = new string[matches.Count];
+
+        // 取得匹配项列表
+        foreach (Match match in matches)
+        {
+            string m = match.Groups["url"].Value;
+            sUrlList[i++] = m;
+            e.Body = e.Body.Replace(m, (Settings.GetSingleValue("qiniu_host") + m).Replace("?", HttpUtility.UrlEncode("?")));
+        }
+        return sUrlList;
+    } 
+}
---
+++ BlogEngine/BlogEngine.NET/Custom/Extensions/AISavePictures.cs
@@ -0,0 +1,208 @@
+﻿/****************************** 模块头 ******************************\
+ * 
+ * 模块名称:  AISavePictures.cs
+ * 创建时间:  2015/2/20
+ * 版权：Copyright (c) (http://zhangx.net) Corporation.
+ * 
+ * 
+ * 实现添加到文章中的本地图片或网络图片自动保存到服务器。
+ * 并且可根据设置大小进行缩略。 
+ * 注意，一些服务器有区分路径大小写， 因此解析路径地址时勿转换大小写。
+ * 
+ * 
+ * This source is subject to the Microsoft Public License.
+ * See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+ * All other rights reserved.
+ * 
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Net;
+
+using BlogEngine.Core.Web.Controls;
+using BlogEngine.Core;
+
+/// <summary>
+/// 插件名称、版本。
+/// </summary>
+/// 
+[Extension("保存图片", "1.0", "<a href=\"http://www.zhangx.net\">zhangx.net</a>")]
+public class AISavePictures
+{
+    // 图片保存路径
+    private static string FOLDER = System.Web.HttpContext.Current.Server.MapPath(BlogConfig.DefaultStorageLocation + "/files/");
+    // 提取出所有图像标签
+    // Matches：<img src="gif.gif" ></img>|<img id="blah" src="gif"></img>|
+    //          <img src="gif.gif" id="freaky">|</img>|<img src="gif.gif" />|
+    //          <img src="gif.gif"/>|<img class="FloatLeft Margin" id="calendarButton81" src="calendar.gif" alt="Calendar Button" />
+    private static string IMAGE_TAG_REGEX = 
+        "<[iI][mM][gG][a-zA-Z0-9\\s=\".]*((src)=\\s*(?:\"([^\"]*)\"|'[^']*'))[a-zA-Z0-9\\s=\".]*/*>(?:</[iI][mM][gG]>)*";
+    //提取出图像src属性
+    // Matches: src="http://www.thoughtlava.com/images/logo.gif"
+    private static string IMAGE_SRC_REGEX = "src=(?:\"|\')?(?<imgSrc>[^>]*[^/].(?:jpg|bmp|gif|png))(?:\"|\')?";
+    private static int Width = 700;
+    private static int Height = 500;
+
+    public AISavePictures()
+    {
+        Post.Serving += new EventHandler<ServingEventArgs>(Post_Serving);
+    }
+
+    void Post_Serving(object sender, ServingEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Body))
+        {
+            if (e.Location == ServingLocation.PostList || e.Location == ServingLocation.SinglePost)
+            {
+                
+                MatchCollection matches = Regex.Matches(e.Body, IMAGE_TAG_REGEX);
+
+                if (matches.Count > 0)
+                {
+                    string filePath = string.Empty;
+                    string newFilePath = string.Empty;
+                    string imageTag = string.Empty;
+                    string url = string.Empty;
+                    FileInfo fi = null;// 需操作的图片文件
+
+                    foreach (Match match in matches)
+                    {
+                        Regex r = new Regex(IMAGE_SRC_REGEX, RegexOptions.Compiled);
+                        Match m = r.Match(match.Value);
+                        if (m.Success)
+                        {
+                            if (m.Value.Contains("http://"))
+                            {
+                                // 网络图片保存到本地
+                                using (WebClient client = new WebClient())
+                                {
+                                    Post post = (Post)sender;
+                                    url = m.Value.Replace("src=", string.Empty).Replace("\"", string.Empty).Trim();
+                                    filePath = post.DateCreated.ToString("yyyy") + "/" + post.DateCreated.ToString("MM") + "/" +
+                                               url.Substring(url.LastIndexOf("/") + 1);
+                                    fi = new FileInfo(FOLDER + filePath);
+
+                                    if (!fi.Exists)
+                                    {
+                                        if (!Directory.Exists(Path.GetDirectoryName(fi.FullName)))
+                                            Directory.CreateDirectory(Path.GetDirectoryName(fi.FullName));
+                                        client.DownloadFile(url, fi.FullName);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                // 本地图片
+                                filePath = m.Value.Replace("src=\"/image.axd?picture=", string.Empty).Replace("\"", string.Empty).Trim();
+                                fi = new FileInfo(FOLDER + filePath);
+                            }
+
+                            // 是否生成缩略图
+                            if (fi.Exists)
+                            {
+                                //Bitmap originalBmp = new Bitmap(fi.FullName);
+
+                                //if (originalBmp.Width < Width && originalBmp.Height < Height)
+                                //{
+                                //    return;
+                                //}
+                                //else
+                                //{
+                                //    newFilePath = filePath.Replace(Path.GetFileName(fi.FullName),
+                                //                      Path.GetFileNameWithoutExtension(fi.FullName) + "-Thumbnail" + Path.GetExtension(fi.FullName));
+                                //    FileInfo newFi = new FileInfo(FOLDER + newFilePath);
+                                //    if (!newFi.Exists)
+                                //    {
+                                //        Bitmap imgTag = CreateThumbnail(originalBmp, Width, Height);
+
+                                //        //Configure JPEG Compression Engine
+                                //        System.Drawing.Imaging.EncoderParameters encoderParams = new System.Drawing.Imaging.EncoderParameters();
+                                //        long[] quality = new long[1];
+                                //        quality[0] = 75;//设定压缩的级别
+
+                                //        System.Drawing.Imaging.EncoderParameter encoderParam = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+                                //        encoderParams.Param[0] = encoderParam;
+
+                                //        System.Drawing.Imaging.ImageCodecInfo[] arrayICI = System.Drawing.Imaging.ImageCodecInfo.GetImageEncoders();
+                                //        System.Drawing.Imaging.ImageCodecInfo jpegICI = null;
+                                //        for (int x = 0; x < arrayICI.Length; x++)
+                                //        {
+                                //            if (arrayICI[x].FormatDescription.Equals("JPEG"))
+                                //            {
+                                //                jpegICI = arrayICI[x];
+                                //                break;
+                                //            }
+                                //        }
+
+                                //        if (jpegICI != null)
+                                //        {
+                                //            imgTag.Save(newFi.FullName, jpegICI, encoderParams);
+                                //        }
+                                //        else
+                                //        {
+                                //            imgTag.Save(newFi.FullName);
+                                //        }
+                                //        imgTag.Dispose();
+                                //    }
+                                //}
+                                //originalBmp.Dispose();
+
+                                imageTag = string.Format("<img alt=\"\" src=\"/image.axd?picture={0}\" />", filePath);
+                                e.Body = e.Body.Replace(match.Value, imageTag);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 创建一个现有图像的缩略图。设置缩略图的最大尺寸高度和尺度等方面，以保持纵横比。
+    /// </summary>
+    /// <param name="imageStream">为创建缩略图的流</param>
+    /// <param name="desiredWidth">理想的宽度</param>
+    /// <param name="desiredHeight">理想的高度</param>
+    /// <returns>缩略图位图</returns>
+    public Bitmap CreateThumbnail(Bitmap originalBmp, int desiredWidth, int desiredHeight)
+    {
+        int newWidth, newHeight;
+
+        if (desiredWidth * originalBmp.Height < desiredHeight * originalBmp.Width)
+        {
+            newWidth = desiredWidth;
+            newHeight = (int)Math.Round((decimal)originalBmp.Height * desiredWidth / originalBmp.Width);
+        }
+        else
+        {
+            newHeight = desiredHeight;
+            newWidth = (int)Math.Round((decimal)originalBmp.Width * desiredHeight / originalBmp.Height);
+        }
+
+        Bitmap bmpOut = new Bitmap(newWidth, newHeight);
+        using (Graphics graphics = Graphics.FromImage(bmpOut))
+        {
+            // 设置高质量插值法 
+            //graphics.InterpolationMode = InterpolationMode.High;
+            // 设置高质量,低速度呈现平滑程度 
+            //graphics.SmoothingMode = SmoothingMode.HighQuality;
+            // 清空画布并以透明背景色填充
+            //graphics.Clear(System.Drawing.Color.Transparent);
+
+            graphics.FillRectangle(Brushes.White, 0, 0, newWidth, newHeight);
+            graphics.DrawImage(originalBmp, 0, 0, newWidth, newHeight);
+        }
+
+        return bmpOut;
+    }
+}
